28df73d617502eae9b0e34c048fcee472a620c07
sbbic <closedevice@gmail.com>
2016-11-04 11:56:34 +0800

    fixed bspatch

--- app/src/main/jni/bspatch_util.c
+++ app/src/main/jni/bspatch_util.c
@@ -2,14 +2,21 @@
 // Created by God on 2016/10/25.
 //
 
-JNIEXPORT jintJNICALL Java_com_closedevice_fastapp_util_BsPatchUtil_patch
+JNIEXPORT jint
+JNICALL Java_com_closedevice_fastapp_util_BsPatchUtil_patch
         (JNIEnv *env, jclass clazz, jstring old, jstring new, jstring patch){
-    int args = 4;
-    char *argv[args];
+int args = 4;
+char *argv[args];
     argv[0] = "bspatch";
-    argv[1] = (char *) ((*env)->GetStringUTFChars(env, old, 0));
-    argv[2] = (char *) ((*env)->GetStringUTFChars(env, new, 0));
-    argv[3] = (char *) ((*env)->GetStringUTFChars(env, patch, 0));
+argv[1] = (char*)((*env)->
+GetStringUTFChars(env, old,
+0));
+argv[2] = (char*)((*env)->
+GetStringUTFChars(env, new,
+0));
+argv[3] = (char*)((*env)->
+GetStringUTFChars(env, patch,
+0));
 
     int ret = executePatch(args, argv);
 
